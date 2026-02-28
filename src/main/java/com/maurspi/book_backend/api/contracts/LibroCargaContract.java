103d739d41ed55de17f97efd2bde6ed644851f0f
maurspi <mauriciospirilis@gmail.com>
2026-02-28 14:30:05 -0300

    Contracto y controlador

---
+++ src/main/java/com/maurspi/book_backend/api/contracts/LibroCargaContract.java
@@ -0,0 +1,19 @@
+package com.maurspi.book_backend.api.contracts;
+
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+@RequestMapping("/api/libros")
+public interface LibroCargaContract {
+
+    /**
+     * Contrato para la subida de archivos CSV.
+     * Define que este endpoint consume 'multipart/form-data' (archivos).
+     */
+    @PostMapping(value = "/subir", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    ResponseEntity<String> subir(@RequestParam("file") MultipartFile file);
+}
